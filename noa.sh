9f2f3919d9be70ebef989e3d3c2eb3d7910ae072
devops <noa.sheyn1@gmail.com>
2019-12-19 10:19:14 +0200

    Fixing script

--- noa.sh
+++ noa.sh
@@ -1,51 +1,32 @@
 #!/bin/bash
 function helpNofication
 {
-    echo "No argument supplied"
+    echo "$# arguments"
 
 }
 
-echo 'Please enter number of files'
-read number
-for ((i = 1; i <= number; i++))
-do
-if (  $# < 1 );
-then
-    exit 1
-
+daydate=$(date '+%d%m%Y')
+DIRNAME="/tmp/test"
+if [ "$#" -lt 1 ];then
+       	echo "Need at least 1 file name"
+	exit 1
+fi
+if [ -d ${DIRNAME} ]; then
+	echo "Directory ${DIRNAME} exists"
+else
+        mkdir ${DIRNAME}
 fi
 
-daydate=$(date '+%d%m%Y')
-        read -p 'NAME: ' NAME
-        echo
-        echo create newfile : $NAME
-	helpNofication
-
-    NAME="${daydate}_${NAME}.txt"
-    dir=/tmp/test/${NAME}
-
-	if [ -d ${NAME} ]
-	then
-        mkdir ${dir}
-        fi
-
-    echo "Creating file: ${NAME}"
-
-	if [ -z "$NAME" ]
-	 then
- 	 helpNofication
-	else
-        echo "create $NAME"
-
-	fi
-    touch /tmp/test/${NAME}
-
+for FILENAME in "$@"
+do
+	 CreatedFile="${FILENAME}_${daydate}.txt"
+	 echo "Creating file: ${CreatedFile}"
+    	touch ${DIRNAME}/${FILENAME}
         if [ $? -eq 0 ]
         then
                 echo "Successfully created file"
         else
                 echo "Could not create file" >&2
         fi
-    # echo $(cat /dev/urandom | tr -dc 'A-Z' | fold -w 3 | head -n 1)$date > "File${i}.txt"
-done
 
+done
